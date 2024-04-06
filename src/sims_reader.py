6a4ee4600bded00e86d903da46a98a69d2ead28f
Nafaryus27 <81865480+Nafaryus27@users.noreply.github.com>
2024-04-06 23:36:48 +0200

    Parser (#3)
    * Parser implementation : Data section complete. Meta csv and subsections not done.
    
    * Parser and semantic implementation complete
    
    * added handling of CALIBRATION PARAMETERS section

---
+++ src/sims_reader.py
@@ -0,0 +1,66 @@
+import tatsu
+
+_str2float = lambda s: float(s or 0)
+
+class Semantic:
+    def __init__(self):
+        pass
+
+    def meta_section_line(self, ast):
+        return [ast.param, ast.value]
+
+    def meta_subsection(self, ast):
+        return(ast.name, dict(ast.lines))
+        
+    def meta_section(self, ast):
+        params = dict(ast.body.lines)
+        if ast.body.subsections:
+            subsections = dict(ast.body.subsections)
+            params.update(subsections)
+        return params
+
+    def calib_species_subsection(self, ast):
+        return [ast.name, dict(ast.params)]
+
+    def calib_param_section(self, ast):
+        section_name = ast.header.lower()
+        params = dict(ast.body.lines)
+        species = {section_name: dict(ast.body.species)}
+        params.update(species)
+        return params
+
+    def meta_csv_section(self, ast):
+        section_name = ast.header.section_name.lower()
+        data = ast.data
+        return {section_name: data}
+
+    def data_section(self, ast):
+        body = ast.body
+        sample = body.sample
+        raw_data = [list(map(_str2float, row)) for row in body.data]
+
+        data_dict = dict()
+        data_header = body.data_header
+        for i,e in enumerate(body.table_header):
+            elem = e[0]
+            nb_columns = len(e[1])
+            values = lambda j : [row[i+j] for row in raw_data]
+            data_dict[elem] = {data_header[i+j]:values(j) for j in range(nb_columns)}
+        return(data_dict)
+
+    def start(self, ast):
+        sections = ast.sections
+        metadata = {}
+        data = {}
+        for section in sections:
+            if "metadata" in section:
+                metadata.update(section["metadata"])
+            if "data" in section:
+                data.update(section["data"])
+        return {"data":data,"metadata":metadata}
+
+def parse_with_semantic(data, grammar):
+    parser = tatsu.compile(grammar)
+    ast = parser.parse(data, semantics=Semantic())
+    return(ast)
+
